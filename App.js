6c90514618b51663b63632e2bd17ad29b7f052ae
retelesk <tranhongquan.jb@gmail.com>
2025-09-13 14:55:26 +0700

    init

--- App.js
+++ App.js
@@ -1,55 +1,62 @@
 import React from "react";
-import {Button, Image, StyleSheet, Text, View} from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 const FinancialManagerApp = () => {
-    return (
-        <View style={styles.container}>
-            <Image style={styles.logo} source={require('./assets/wallet-solid-full.png')}/>
-            <Text style={styles.welcomeText}>Finance Manager App</Text>
-            <View style={styles.buttonContainer}>
-                <Button color="#25215E"
-                        title="Login"
-                        onPress={() => {
-                            alert("Login");
-                        }}
-                />
-                <Button color="#25215E" title="Register" onPress={() => {
-                    alert("Register");
-                }}/>
-            </View>
-
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("./src/assets/icons/wallet-solid-full.png")}
+      />
+      <Text style={styles.welcomeText}>Finance Manager App</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          color="#25215E"
+          title="Login"
+          onPress={() => {
+            alert("Login");
+          }}
+        />
+        <Button
+          color="#25215E"
+          title="Register"
+          onPress={() => {
+            alert("Register");
+          }}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFFFFF",
-        rowGap: 20,
-    },
-    welcomeText: {
-        color: "#25215E",
-        fontWeight: "bold",
-        fontSize: 22,
-        textTransform: "uppercase",
-    },
-    logo: {
-        width: 60,
-        height: 60,
-        resizeMode: "contain",
-    },
-    button: {
-        color: '#FBAC42'
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        width: "60%",
-        columnGap: 10,
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    rowGap: 20,
+  },
+  welcomeText: {
+    color: "#25215E",
+    fontWeight: "bold",
+    fontSize: 22,
+    textTransform: "uppercase",
+  },
+  logo: {
+    width: 60,
+    height: 60,
+    resizeMode: "contain",
+  },
+  button: {
+    color: "#FBAC42",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "60%",
+    columnGap: 10,
+  },
 });
 
 export default FinancialManagerApp;
