b03b716426bcbc3e0a80becb3e57a2de09a95507
retelesk <tranhongquan.jb@gmail.com>
2025-10-10 11:23:53 +0700

    cập nhật stack

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,129 +1,129 @@
 import React from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { styles } from "../../utils/Styles";
+import {Image, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {styles} from "../../utils/Styles";
 import validator from "validator";
-import { Alert } from "react-native";
-import { authService } from "../../services/auth/authServices.js";
+import {authService} from "../../services/auth/authServices.js";
 
-const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [loading, setLoading] = React.useState(false);
-  const [error, setError] = React.useState("");
+const LoginScreen = ({navigation}) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+    const [emailError, setEmailError] = React.useState("");
+    const [passwordError, setPasswordError] = React.useState("");
 
-  const validateEmail = (email) => {
-    if (!email) {
-      setError("Email không đươợc để trống");
-      return false;
-    }
-    if (!validator.isEmail(email)) {
-      setError("Email không hợp lệ");
-      return false;
-    }
-    setError("");
-    return true;
-  };
+    const validateEmail = (email) => {
+        if (!email) {
+            setEmailError("Email không được để trống");
+            return false;
+        }
+        if (!validator.isEmail(email)) {
+            setEmailError("Email không hợp lệ");
+            return false;
+        }
+        setEmailError("");
+        return true;
+    };
 
-  const handleEmailChange = (text) => {
-    setEmail(text);
-    if (error && text) {
-      setError("");
-    }
-  };
-  const handleLogin = async () => {
-    if (!validateEmail(email)) {
-      return;
-    }
-    if (!password) {
-      setError("Mật khẩu không được để trống");
-    }
-    if (password.length < 8) {
-      setError("Mật khẩu phải có ít nhất 8 ký tự");
-    }
-    setLoading(true);
-    setError("");
+    const handleEmailChange = (text) => {
+        setEmail(text);
+        if (emailError && text) {
+            setEmailError("");
+        }
+    };
+    const handleLogin = async () => {
+        if (!validateEmail(email)) {
+            return;
+        }
+        if (!password) {
+            setPasswordError("Mật khẩu không được để trống");
+        }
+        if (password.length < 8) {
+            setPasswordError("Mật khẩu phải có ít nhất 8 ký tự");
+        }
+        setLoading(true);
+        setPasswordError("");
 
-    try {
-      const response = await authService.login(email, password);
-      navigation.reset({
-        index: 0,
-        routes: [{ name: "DashboardScreen" }],
-      });
-    } catch (error) {
-      let errorMessage = "Đăng nhập thất bại";
-      if (error.message) {
-        errorMessage = error.message;
-      } else if (typeof error === "string") {
-        errorMessage = error;
-      } else if (error.error) {
-        errorMessage = error.error;
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+        try {
+            const response = await authService.login(email, password);
+            navigation.reset({
+                index: 0,
+                routes: [{name: "DashboardScreen"}],
+            });
+        } catch (error) {
+            let errorMessage = "Đăng nhập thất bại";
+            if (error.message) {
+                errorMessage = error.message;
+            } else if (typeof error === "string") {
+                errorMessage = error;
+            } else if (error.error) {
+                errorMessage = error.error;
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image
-          style={styles.logo}
-          source={require("../../assets/icons/wallet-solid-full.png")}
-        />
-        <Text style={styles.welcomeText}>BudgetNest</Text>
-      </View>
-      {error ? <Text style={styles.errorText}>{error}</Text> : null}
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputTitle}>Email</Text>
-        <TextInput
-          style={[styles.input, error ? styles.inputError : null]}
-          value={email}
-          onChangeText={setEmail}
-          onChange={handleEmailChange}
-          onBlur={() => {
-            validateEmail(email);
-          }}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          editable={!loading}
-        />
-        <Text style={styles.inputTitle}>Mật khẩu</Text>
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry={true}
-          editable={!loading}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={[styles.mainButton, loading && styles.disabledButton]}
-          onPress={handleLogin}
-          disabled={loading}
-        >
-          <Text style={styles.mainButtonText}>Đăng nhập</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("ForgotPasswordScreen");
-          }}
-        >
-          <Text style={styles.subButtonText}>Quên mật khẩu</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.footerContainer}>
-        <Text style={styles.inputTitle}>Chưa có tài khoản?</Text>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("RegisterScreen");
-          }}
-        >
-          <Text style={styles.subButtonText}>Đăng ký</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require("../../assets/icons/wallet-solid-full.png")}
+                />
+                <Text style={styles.welcomeText}>BudgetNest</Text>
+            </View>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            <View style={styles.inputContainer}>
+                <Text style={styles.inputTitle}>Email</Text>
+                <TextInput
+                    style={[styles.input, error ? styles.inputError : null]}
+                    value={email}
+                    onChangeText={setEmail}
+                    onChange={handleEmailChange}
+                    onBlur={() => {
+                        validateEmail(email);
+                    }}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    editable={!loading}
+                />
+                <Text style={styles.inputTitle}>Mật khẩu</Text>
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={true}
+                    editable={!loading}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={[styles.mainButton, loading && styles.disabledButton]}
+                    onPress={handleLogin}
+                    disabled={loading}
+                >
+                    <Text style={styles.mainButtonText}>Đăng nhập</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("ForgotPasswordScreen");
+                    }}
+                >
+                    <Text style={styles.subButtonText}>Quên mật khẩu</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.footerContainer}>
+                <Text style={styles.inputTitle}>Chưa có tài khoản?</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("RegisterScreen");
+                    }}
+                >
+                    <Text style={styles.subButtonText}>Đăng ký</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 };
 
 export default LoginScreen;
--- src/services/api/api.js
+++ src/services/api/api.js
@@ -4,34 +4,34 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const API_BASE_URL = "https://budget.retelesk.synology.me/api/v1";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: API_BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use(
-  async (config) => {
-    const token = await AsyncStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    async (config) => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      console.log("Token đã hết hạn, vui lòng đăng nhập lại.");
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            console.log("Token đã hết hạn, vui lòng đăng nhập lại.");
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 export default api;
