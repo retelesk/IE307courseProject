4fb87d178a2b5ce1bba3652ffc3d13a037f475ca
retelesk <tranhongquan.jb@gmail.com>
2025-10-17 22:06:32 +0700

    Sửa lại stack

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 // Navigator chính
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import DashboardScreen from "../screens/main/DashboardScreen";
-import BudgetScreen from "../screens/main/BudgetScreen";
+import AddBills from "../screens/main/AddBills";
 import UserScreen from "../screens/main/UserScreen";
 import TransactionsScreen from "../screens/main/TransactionsScreen";
 import ReportsScreen from "../screens/main/ReportsScreen";
@@ -11,8 +11,8 @@ export default function AppNavigator() {
     return (
         <Tab.Navigator id="AppNavigator" screenOptions={{headerShown: false}}>
             <Tab.Screen name="DashboardScreen" component={DashboardScreen} options={{headerShown: false}}/>
-            <Tab.Screen name="BudgetScreen" component={BudgetScreen} options={{headerShown: false}}/>
             <Tab.Screen name="ReportsScreen" component={ReportsScreen} options={{headerShown: false}}/>
+            <Tab.Screen name="AddBills" component={AddBills} options={{headerShown: false}}/>
             <Tab.Screen name="TransactionsScreen" component={TransactionsScreen} options={{headerShown: false}}/>
             <Tab.Screen name="UserScreen" component={UserScreen} options={{headerShown: false}}/>
         </Tab.Navigator>
