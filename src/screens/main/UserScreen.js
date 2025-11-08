8fd196d11f496efcabdb57a1ef505c2aea0d935c
retelesk <tranhongquan.jb@gmail.com>
2025-11-08 16:58:41 +0700

    Bổ sung trang giao diện người dùng và tính năng đăng xuất

--- src/screens/main/UserScreen.js
+++ src/screens/main/UserScreen.js
@@ -1,3 +1,79 @@
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useAuth} from "../../contexts/AuthContext";
+import {authService} from "../../services/auth/authServices";
+import React from "react";
+
 export default function UserScreen() {
-    return null;
+    const {user, signOut} = useAuth();
+    const [loading, setLoading] = React.useState(false);
+
+
+    const handleLogout = async () => {
+        setLoading(true);
+        try {
+            await authService.logout();
+            await signOut();
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+    return <View style={styles.viewWrapper}>
+        <View style={styles.userInfoBox}>
+            <View style={styles.userInfo}>
+                <Text style={styles.userInfoText}>FullName: {user ? user.fullname : ""}</Text>
+            </View>
+            <View style={styles.userInfo}>
+                <Text style={styles.userInfoText}>Email: {user ? user.email : ""}</Text>
+            </View>
+            <View style={styles.userInfo}>
+                <Text style={styles.userInfoText}>Phone: {user ? user.phone : ""}</Text>
+            </View>
+            <View style={styles.userInfo}>
+                <Text style={styles.userInfoText}>Address: {user ? user.address : ""}</Text>
+            </View>
+            <View style={styles.userInfo}>
+                <Text style={styles.userInfoText}>Role: {user ? user.role : ""}</Text>
+            </View>
+
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+            <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+    </View>;
 }
+
+const styles = StyleSheet.create({
+    viewWrapper: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    userInfoBox: {
+        width: '80%',
+        backgroundColor: '#f0f0f0',
+        borderRadius: 5,
+    },
+    userInfo: {
+        marginBottom: 10,
+        padding: 10,
+        backgroundColor: '#ffffff',
+        borderRadius: 5,
+    },
+    userInfoText: {
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: '#25215E',
+        padding: 10,
+        marginTop: 10,
+        borderRadius: 5,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+});
--- src/services/auth/authServices.js
+++ src/services/auth/authServices.js
@@ -2,49 +2,49 @@ import api from "../api/api.js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const authService = {
-  login: async (email, password) => {
-    try {
-      const response = await api.post("/users/login", {
-        email,
-        password,
-      });
+    login: async (email, password) => {
+        try {
+            const response = await api.post("/users/login", {
+                email,
+                password,
+            });
 
-      const { token, user } = response.data || {};
+            const {token, user} = response.data || {};
 
-      if (token) {
-        await AsyncStorage.setItem("userToken", token);
-      }
+            if (token) {
+                await AsyncStorage.setItem("userToken", token);
+            }
 
-      if (user) {
-        await AsyncStorage.setItem("userData", JSON.stringify(user));
-      } else {
-        await AsyncStorage.removeItem("userData");
-      }
+            if (user) {
+                await AsyncStorage.setItem("userData", JSON.stringify(user));
+            } else {
+                await AsyncStorage.removeItem("userData");
+            }
 
-      return response.data;
-    } catch (error) {
-      throw error.response?.data || error.message;
-    }
-  },
+            return response.data;
+        } catch (error) {
+            throw error.response?.data || error.message;
+        }
+    },
 
-  // Dang xu?t
-  logout: async () => {
-    try {
-      await AsyncStorage.removeItem("userToken");
-      await AsyncStorage.removeItem("userData");
-    } catch (error) {
-      console.error("Logout error:", error);
-    }
-  },
+    // Đăng xuất
+    logout: async () => {
+        try {
+            await AsyncStorage.removeItem("userToken");
+            await AsyncStorage.removeItem("userData");
+        } catch (error) {
+            console.error("Logout error:", error);
+        }
+    },
 
-  // Lấy thông tin user
-  getCurrentUser: async () => {
-    try {
-      const userData = await AsyncStorage.getItem("userData");
-      return userData ? JSON.parse(userData) : null;
-    } catch (error) {
-      console.error("Get user error:", error);
-      return null;
-    }
-  },
+    // Lấy thông tin user
+    getCurrentUser: async () => {
+        try {
+            const userData = await AsyncStorage.getItem("userData");
+            return userData ? JSON.parse(userData) : null;
+        } catch (error) {
+            console.error("Get user error:", error);
+            return null;
+        }
+    },
 };
